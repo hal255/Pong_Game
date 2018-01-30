f71dee4153345413d7becf712e0290d9d9c7bf84
Hong Luu <hluu114@gmail.com>
2018-01-29 18:43:41 -0800

    Initial Push

---
+++ Assets/Scripts/Pong_Ball_Motion.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pong_Ball_Motion : MonoBehaviour {
+    private bool debug = true;
+
+    [SerializeField]
+    float move_speed = 10.0f;               // velocity of ball
+    private float rotate_angle = 0.0f;      // angle of ball, from top down view
+
+    //float added_speed = 0.0f;               // added speed from user's paddle
+
+	// Use this for initialization
+	void Start () {
+        // generate a random angle and move towards that direction
+        rotate_angle = GetNewAngle(0);
+        SetBallRotation();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Translate(Vector3.forward * move_speed * Time.deltaTime);
+	}
+
+    float GetNewAngle(float angle)
+    {
+        angle = Random.value * 359;
+        if ((angle >= 25f && angle <= 155f) || (angle >= 205f && angle <= 335f))
+            return angle;
+        else
+            return GetNewAngle(angle);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if (debug)
+        //    Debug.Log("Pong ball collided with " + collision.collider.name + "!! current angle: " + rotate_angle);
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        //if (Physics.Raycast(ray, out hit, move_speed * Time.deltaTime, 10))
+        //{
+        //    Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+        //    float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+        //    transform.eulerAngles = new Vector3(0, rot, 0);
+        //}
+        if (Physics.Raycast(ray, out hit, 0.5f))
+        {
+            Debug.Log("Pong ball colliding with: " + hit.collider.name + ", hit distance: " + hit.distance + ", current angle: " + rotate_angle);
+            Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
+            float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rot, 0);
+            //rotate_angle += rot;
+            //SetBallRotation();
+        }
+    }
+
+    void SetBallRotation()
+    {
+        transform.Rotate(Vector3.up * rotate_angle);       // "up" is the y-axis, the top-down view
+        if (debug)
+            Debug.Log("Pong ball new angle: " + rotate_angle);
+    }
+}
---
+++ Assets/Scripts/Pong_Player_Keyboard_Controls.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pong_Player_Keyboard_Controls : MonoBehaviour {
+    private bool debug = false;
+
+    [SerializeField]
+    float move_speed = 1.0f;
+
+    private float modifier = 1.0f;
+    private bool isPlayer01 = true;
+    private bool isWestWallCollided = false;        // if colliding with wall, stop moving in direction of west wall
+    private bool isEastWallCollided = false;        // if colliding with wall, stop moving in direction of east wall
+
+    // Use this for initialization
+    void Start () {
+        if (gameObject.name.Contains("2"))
+        {
+            modifier *= -1;
+            isPlayer01 = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) && isPlayer01)
+        {
+            if (debug)
+                Debug.Log("Pressed player1 left key.");
+            moveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.D) && isPlayer01)
+        {
+            if (debug)
+                Debug.Log("Pressed player1 right key.");
+            moveRight();
+        }
+
+        if (Input.GetKey(KeyCode.J) && !isPlayer01)
+        {
+            if (debug)
+                Debug.Log("Pressed player2 left key.");
+            moveLeft();
+        }
+
+        if (Input.GetKey(KeyCode.L) && !isPlayer01)
+        {
+            if (debug)
+                Debug.Log("Pressed player2 right key.");
+            moveRight();
+        }
+
+    }
+
+    private void moveLeft()
+    {
+        // if colliding with west wall, stop moving west
+        if (!isWestWallCollided)
+        {
+            if (move_speed > 0)
+                move_speed *= -1.0f;
+            transform.Translate(Vector3.forward * move_speed * modifier * Time.deltaTime);
+        }
+    }
+    private void moveRight()
+    {
+        // if colliding with east wall, stop moving east
+        if (!isEastWallCollided)
+        {
+            if (move_speed < 0)
+                move_speed *= -1.0f;
+            transform.Translate(Vector3.forward * move_speed * modifier * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        string collided_name = collision.gameObject.name;
+        Debug.Log(gameObject.name + " collided with " + collided_name);
+
+        if (collided_name.Contains("west"))
+            isWestWallCollided = true;
+        if (collided_name.Contains("east"))
+            isEastWallCollided = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        string collided_name = collision.gameObject.name;
+        Debug.Log(gameObject.name + " no longer collides with " + collided_name);
+
+        if (collided_name.Contains("west"))
+            isWestWallCollided = false;
+        if (collided_name.Contains("east"))
+            isEastWallCollided = false;
+    }
+}
